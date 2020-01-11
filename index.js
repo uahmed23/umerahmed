85d7b675bdb7b20cf3e2b81122ec8402e4d40409
uahmed23 <umer@uoguelph.ca>
2020-01-10 22:04:36 -0500

    coop 1

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
-    console.log("Hello world");
-});
